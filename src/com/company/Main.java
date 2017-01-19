5b72da5e622c772e1785f00eca26afd64f4b69d1
gbarth1 <grace.m.barth@gmail.com>
2017-01-19 14:30:03 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,32 @@
+package com.company;
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Insert a number to find out if it is apart of the Fibonacci sequence:");
+
+        /*int prev = 1;
+        int curr = 1;
+
+        System.out.print(prev + " ");
+        for (int i = 1; i < 10; i++) {
+            System.out.print(curr + " ");
+            int temp = curr;
+            curr = curr + prev;
+            prev = temp;*/
+        }
+
+    int prev = 1;
+    int curr = 1;
+    public static int fibonacci(int pr, int cu)
+    {
+
+            cu = cu + pr;
+            pr++;
+            return pr;
+
+
+    }
+
+}
